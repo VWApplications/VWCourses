8280e687176e1c9075a833b2de51d32d26686c0b
Victor Arnaud <victorhad@gmail.com>
2017-01-19 04:57:50 -0200

    Create home page

---
+++ vwcourses/core/static/core/core.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+  $(".button-collapse").sideNav();
+  $('.materialboxed').materialbox();
+  $('.parallax').parallax();
+  $('.modal').modal({
+    dismissible: true,
+    opacity: .5,
+    in_duration: 300,
+    out_duration: 200,
+  });
+});
